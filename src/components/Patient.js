4fe56463bd38fa3555aedf3e57417c2cfee535b8
Akash <57212963+Akash-152000@users.noreply.github.com>
2021-04-26 15:35:30 +0530

    Add files via upload

---
+++ src/components/Patient.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import Navbar from './Navbar'
+import Dialog from './Dialog'
+import Buttons from './Buttons'
+
+
+class Patient extends Component {
+
+ 
+  render() {
+
+    return (
+    <div>
+      <Buttons account={this.props.account} 
+                images={this.props.images}
+                captureFile={this.props.captureFile}
+                uploadImage={this.props.uploadImage}
+                buttonFunction={this.buttonFunction}
+                tipImageOwner={this.props.tipImageOwner}/>
+
+    </div>
+
+    );
+  }
+}
+
+export default Patient;
